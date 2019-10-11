566a731cd6a64fa1e42fbd5bc04c95f80f9add41
Nhung <nhungast@gmail.com>
2019-10-11 21:19:54 +0700

    condition applied clean code

--- src/week_2_2.js
+++ src/week_2_2.js
@@ -5,26 +5,28 @@ const viLocale = require('date-fns/locale/vi');
 
 // Use fs.readFile() to read the products.json file and convert it to JS object.
 fs.readFile(path.join(__dirname, '/utils/product.json'), 'utf8', function(err, data) {
-  if (!err) {
-    const products = JSON.parse(data.toString());
-
-    // Print the total number of products to console.
-    console.log('Total number of products ', products.length);
-
-    // Convert dateUpdated of each item into real Date.
-    const updatedProducts = products.map(p => ({
-      ...p,
-      dateUpdated: new Date(p.dateUpdated),
-    }));
-
-    // Print the list to the console with following template for each product.
-    // ${name} - ${price}VND - Cập nhật cách đây: ${fromNow}
-    updatedProducts.forEach(product => {
-      const price = formatMoney(product.price);
-      const fromNow = distanceFromNow(product.dateUpdated);
-      console.log(`${product.id} - ${product.name} - ${price} - Cập nhật cách đây: ${fromNow}`);
-    });
+  if (err) {
+    console.log('err ', err);
   }
+
+  const products = JSON.parse(data.toString());
+
+  // Print the total number of products to console.
+  console.log('Total number of products ', products.length);
+
+  // Convert dateUpdated of each item into real Date.
+  const updatedProducts = products.map(p => ({
+    ...p,
+    dateUpdated: new Date(p.dateUpdated),
+  }));
+
+  // Print the list to the console with following template for each product.
+  // ${name} - ${price}VND - Cập nhật cách đây: ${fromNow}
+  updatedProducts.forEach(product => {
+    const price = formatMoney(product.price);
+    const fromNow = distanceFromNow(product.dateUpdated);
+    console.log(`${product.id} - ${product.name} - ${price} - Cập nhật cách đây: ${fromNow}`);
+  });
 });
 
 const formatMoney = money => {
--- src/week_2_3.js
+++ src/week_2_3.js
@@ -5,28 +5,30 @@ const XLSX = require('xlsx');
 
 // Use fs.readFile() to read the products.json file and convert it to JS object.
 fs.readFile(path.join(__dirname, '/utils/product.json'), 'utf8', function(err, data) {
-  if (!err) {
-    const products = JSON.parse(data.toString());
-
-    // Create new field updated from dateUpdated with following format: MM/DD/YYYY
-    // Delete dateUpdated field
-    const updatedProducts = products.map(p => {
-      const { dateUpdated, ...product } = p;
-      const updated = format(new Date(dateUpdated), 'MM/dd/yyyy');
-      return {
-        ...product,
-        updated,
-      };
-    });
-
-    const worksheet = XLSX.utils.json_to_sheet(updatedProducts);
-    worksheet['!cols'] = [{ width: 15 }, { width: 30 }, { width: 20 }, { width: 20 }, { width: 20 }];
-
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Products');
-
-    const buf = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' });
-
-    fs.writeFile(path.join(__dirname, '/utils/product.xlsx'), buf, err => console.log(err));
+  if (err) {
+    console.log('err ', err);
   }
+
+  const products = JSON.parse(data.toString());
+
+  // Create new field updated from dateUpdated with following format: MM/DD/YYYY
+  // Delete dateUpdated field
+  const updatedProducts = products.map(p => {
+    const { dateUpdated, ...product } = p;
+    const updated = format(new Date(dateUpdated), 'MM/dd/yyyy');
+    return {
+      ...product,
+      updated,
+    };
+  });
+
+  const worksheet = XLSX.utils.json_to_sheet(updatedProducts);
+  worksheet['!cols'] = [{ width: 15 }, { width: 30 }, { width: 20 }, { width: 20 }, { width: 20 }];
+
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Products');
+
+  const buf = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' });
+
+  fs.writeFile(path.join(__dirname, '/utils/product.xlsx'), buf, err => console.log(err));
 });
