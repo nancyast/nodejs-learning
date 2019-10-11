b52947c840071ea9d7f159092e3b0285211933b2
Nhung <nhungast@gmail.com>
2019-10-11 22:19:23 +0700

    remove unused code

--- src/week_2_1.js
+++ src/week_2_1.js
@@ -1,44 +1,27 @@
 const chalk = require('chalk');
 const readline = require('readline');
-// const prompts = require('prompts');
 
-function prompt(question) {
+const prompt = question => {
   const read = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false,
   });
 
-  return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve) {
     read.question(question, function(answer) {
       read.close();
       resolve(answer);
     });
   });
-}
+};
 
 const main = () => {
-  const questions = [
-    {
-      type: 'text',
-      name: 'name',
-      message: `What's your name?`,
-    },
-    {
-      type: 'number',
-      name: 'birthday',
-      message: `What's your year of birth?`,
-    },
-    {
-      type: 'text',
-      name: 'hometown',
-      message: `What's your home town?`,
-    },
-  ];
+  const questions = [`What's your name?`, `What's your year of birth?`, `What's your home town?`];
 
-  prompt(questions[0].message + ' ').then(name => {
-    prompt(questions[1].message + ' ').then(birthday => {
-      prompt(questions[2].message + ' ').then(hometown => {
+  prompt(questions[0] + ' ').then(name => {
+    prompt(questions[1] + ' ').then(birthday => {
+      prompt(questions[2] + ' ').then(hometown => {
         const date = new Date();
         const greet = `Thank you. Hello ${chalk.red(name)}, so you are ${chalk.yellow(
           date.getFullYear() - Number(birthday),
@@ -50,30 +33,3 @@ const main = () => {
 };
 
 main();
-
-// (async function() {
-//   const questions = [
-//     {
-//       type: 'text',
-//       name: 'name',
-//       message: `What's your name?`,
-//     },
-//     {
-//       type: 'number',
-//       name: 'birthday',
-//       message: `What's your year of birth?`,
-//     },
-//     {
-//       type: 'text',
-//       name: 'hometown',
-//       message: `What's your home town?`,
-//     },
-//   ];
-
-//   const answers = await prompts(questions);
-//   const date = new Date();
-//   const greet = `Thank you. Hello ${chalk.red(answers.name)}, so you are ${chalk.yellow(
-//     date.getFullYear() - answers.birthday,
-//   )} years old and from ${chalk.green(answers.hometown)}`;
-//   console.log(greet);
-// })();
